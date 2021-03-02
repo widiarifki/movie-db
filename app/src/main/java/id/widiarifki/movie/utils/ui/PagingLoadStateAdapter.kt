a6edb3c11b0aa249ece10d2a6254f5b40e5e14a0
Widia Rifkianti <widiarifki@gmail.com>
2021-03-02 15:42:26 +0700

    code refactoring

--- app/src/main/java/id/widiarifki/movie/utils/ui/PagingLoadStateAdapter.kt
+++ app/src/main/java/id/widiarifki/movie/utils/ui/PagingLoadStateAdapter.kt
@@ -6,12 +6,12 @@ import androidx.paging.LoadState
 import androidx.paging.LoadStateAdapter
 import androidx.recyclerview.widget.RecyclerView
 import id.widiarifki.movie.R
-import id.widiarifki.movie.databinding.BaseLoadingErrorBinding
+import id.widiarifki.movie.databinding.BaseLoadingStateBinding
 
 class PagingLoadStateAdapter(private val retryCallback: () -> Unit)
     : LoadStateAdapter<PagingLoadStateAdapter.LoadStateViewHolder>() {
 
-    class LoadStateViewHolder(val binding: BaseLoadingErrorBinding) : RecyclerView.ViewHolder(binding.root)
+    class LoadStateViewHolder(val binding: BaseLoadingStateBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onBindViewHolder(holder: LoadStateViewHolder, loadState: LoadState) {
         val viewBinding = holder.binding
@@ -29,7 +29,7 @@ class PagingLoadStateAdapter(private val retryCallback: () -> Unit)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): LoadStateViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.base_loading_error, parent, false)
-        return LoadStateViewHolder(BaseLoadingErrorBinding.bind(view))
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.base_loading_state, parent, false)
+        return LoadStateViewHolder(BaseLoadingStateBinding.bind(view))
     }
 }
